e2223866ceefb5ba74698f0d4d0d7711596f6411
prash <87913082+codewithprash@users.noreply.github.com>
2022-12-09 18:41:25 +0530

    1.2.1

---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tap to Chat',
+      theme: ThemeData(
+        primaryColor: const Color(0xFF075e54),
+        primarySwatch: Colors.green,
+      ),
+      home: const Dash(title: 'OnTap'),
+    );
+  }
+}
+
+class Dash extends StatefulWidget {
+  const Dash({super.key, required this.title});
+  final String title;
+
+  @override
+  State<Dash> createState() => _DashState();
+}
+
+class _DashState extends State<Dash> {
+  final TextEditingController _mobnum = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: const Color(0xFF128C7F),
+        title: Text(
+          widget.title,
+          style: Theme.of(context).textTheme.headline4,
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(40.0),
+              child: Text(
+                'Send WhatsApp messages without saving phone number',
+                style: Theme.of(context).textTheme.headline5,
+                overflow: TextOverflow.visible,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: TextField(
+                controller: _mobnum,
+                decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Color(0xFF128C7F)),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    hintText: 'Enter WhatsApp Number',
+                    prefixText: "+91",
+                    filled: true),
+                keyboardType: TextInputType.number,
+                textInputAction: TextInputAction.done,
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                String num = _mobnum.text.trim();
+
+                String url =
+                    'https://api.whatsapp.com/send/?phone=91$num&text=Hi';
+
+                if (await canLaunchUrlString(url)) {
+                  await launchUrlString(url);
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFF128C7F),
+                textStyle: const TextStyle(fontSize: 16),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                shape: const StadiumBorder(),
+              ),
+              child: const Text("Continue to Chat"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
